5b62e454c981bfba44b09015a4e19a3c942258aa
pnkosev <pnikolaykosev@gmail.com>
2020-01-10 11:48:12 +0100

    Added comment

--- 08.GenericsLab/src/arrayCreator/Main.java
+++ 08.GenericsLab/src/arrayCreator/Main.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        Object[] stringArr = ArrayCreator.create(10, "none");
+//        String[] stringArr = ArrayCreator.create(10, "none");             NOT WORKING SINCE IT CANNOT CAST TO STRING[]
+//        Integer[] intArr = ArrayCreator.create(5, 9);                     NOT WORKING SINCE IT CANNOT CAST TO INTEGER[]
+        String[] stringArr = ArrayCreator.create(10, "none");
         Object[] intArr = ArrayCreator.create(5, 9);
 
         String[] yetAnotherStringArr = ArrayCreator.create(String.class, 10, "none");
