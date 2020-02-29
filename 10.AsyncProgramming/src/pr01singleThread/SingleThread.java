722aba592d8df190992cee15338e1751ef5c779f
PN <pnikolaykosev@gmail.com>
2020-02-29 15:56:02 +0100

    Added solutions

--- 10.AsyncProgramming/src/pr01singleThread/SingleThread.java
+++ 10.AsyncProgramming/src/pr01singleThread/SingleThread.java
@@ -1,4 +1,19 @@
-package pr01singleThread;
+package p01singleThread;
 
 public class SingleThread {
+    public static void main(String[] args) throws InterruptedException {
+
+        Runnable task = () -> {
+            for (int i = 1; i <= 10; i++) {
+                System.out.printf("%d ", i);
+            }
+//            while (true) {}
+        };
+
+        Thread thread = new Thread(task);
+
+        thread.start();
+        thread.join(); // blocks the calling thread so it can wait the called thread to finish execution
+        System.exit(1);
+    }
 }
--- 10.AsyncProgramming/src/pr02multiThread/MultiThread.java
+++ 10.AsyncProgramming/src/pr02multiThread/MultiThread.java
@@ -1,4 +1,25 @@
-package pr02multiThread;
+package p02multiThread;
 
 public class MultiThread {
+    public static void main(String[] args) throws InterruptedException {
+        Runnable task = () -> {
+            for (int i = 1; i <= 10; i++) {
+                System.out.printf("[%d] ", i);
+                Thread.yield(); // causes the currently executing thread object to temporarily pause and allow other threads to execute. HOWEVER IT IS NOT CERTAIN!!!
+            }
+        };
+
+        Thread[] threads = new Thread[5];
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(task);
+            threads[i].start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        System.exit(1);
+    }
 }
